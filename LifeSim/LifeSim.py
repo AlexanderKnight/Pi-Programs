c83664f9870193bc3fd8f20c37bac3711a3ae4f3
Alexander Knight <alexander.knighr@maine.edu>
2016-05-19 12:54:09 -0400

    Changes to lifesim.py

--- LifeSim/LifeSim.py
+++ LifeSim/LifeSim.py
@@ -6,48 +6,51 @@ Created on Wed May 18 09:12:43 2016
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import random
 
 
-class sHerb:
+class Animal:
     '''
-    class for a small herbivore (rabbit)
+    class for general animal
     '''
     
     def __init__(self, size=10.0, walk=2.0, run=10.0, \
     energyTotal=100.0, energy = 100.0, walkEn=0.2, runEn=1.0, \
-    lifespan=500, life= 500, x=0, y=0, mate=False):
+    lifespan=500, life= 500, x=0, y=0, inHeat=False):
         
-        self.size = size #How large it is
-        self.walk = walk #How fast is can walk
-        self.run = run #How fast it can run
-        self.energyTotal = energyTotal #How much total energy it can have
-        self.energy = energyTotal #how much energy is currently has
-        self.walkEn = walkEn #How much energy it takes to walk one step
-        self.runEn = runEn #How much energy is takes to run one step
-        self.lifespan = lifespan
-        self.life = life #How many time steps it will live
+        self.size = np.random.normal(size) #How large it is
+        self.walk = np.random.normal(walk) #How fast is can walk
+        self.run = np.random.normal(run) #How fast it can run
+        self.energyTotal = np.random.normal(energyTotal) #How much total energy it can have
+        self.energy = np.random.normal(energyTotal) #how much energy is currently has
+        self.walkEn = np.random.normal(walkEn) #How much energy it takes to walk one step
+        self.runEn = np.random.normal(runEn) #How much energy is takes to run one step
+        self.lifespan = np.random.normal(lifespan) #How long it will live
+        self.life = np.random.normal(life) #How many time steps it has
         self.x = x #It's x axis position
         self.y = y #It's y axis position
+        self.inHeat = inHeat #if it is in heat and willing to breed
     
-    def move(self, n, m, speed ='walk'):
+    def move(self, x=0, y=0, isRunning = False):
         '''
-        one step in a direction
+        one step in a direction, sl
         '''
         
-        if speed == 'walk':
-            self.x += n*self.walk
-            self.y += m*self.walk
+        if isRunning == False:
+            self.x += x*self.walk
+            self.y += y*self.walk
             
-        elif speed == 'run':
-            self.x += n*self.run
-            self.y += m*self.run
+        elif isRunning == True:
+            self.x += x*self.run
+            self.y += y*self.run
             
     def breed(self, other):
         '''
         breeds two animals, generating child with random traits based on
-        normal distrobution with average of parent's traits
+        normal distrobution with average of parent's traits (allows for mutations)
         '''
-        baby = sHerb()
+        baby = type(self)()
         baby.size = np.random.normal(loc=(self.size+ other.size)/2)
         baby.walk = np.random.normal((self.walk+other.walk)/2)
         baby.run = np.random.normal((self.run+other.run)/2)
@@ -61,57 +64,6 @@ class sHerb:
         return baby
         
         
-        
-        
-class lHerb:
-    '''
-    Class for a large herbivore (deer)
-    '''
-    
-    def __init__(self, size=100.0, walk=3.0, run=15.0, \
-    energyTotal=500.0, energy = 500.0, walkEn=0.4, runEn=2.0,\
-    lifespan=5000, life=5000, x=0, y=l0, mate=False):
-        
-        self.size = size #How large it is
-        self.walk = walk #How fast it walks
-        self.run = run #How fast it runs
-        self.energy = energy #How much total energy it can have
-        self.walkEn = walkEn #How much energy it takes to walk one step
-        self.runEn = runEn # How much energy it takes to run one step
-        self.lifespan = lifespan #How many time steps it will live
-        self.life = lifespan #How much life it currently has
-        self.x = x #Its x-axis position
-        self.y = y #Its y-axis position
-        
-    def move(self, n, m, speed = 'walk'):
-        
-        if speed == 'walk':
-            self.x += n*self.walk
-            self.y += m*self.walk
-            
-        elif speed == 'run':
-            self.x += n*self.run
-            self.y += m*self.run
-            
-    def breed(self, other):
-       '''
-       breeds two animals, generating child with random traits based on
-       normal distrobution with average of parent's traits
-       '''
-       baby = lHerb()
-       baby.size = np.random.normal(loc=(self.size+ other.size)/2)
-       baby.walk = np.random.normal((self.walk+other.walk)/2)
-       baby.run = np.random.normal((self.run+other.run)/2)
-       baby.energyTotal = np.random.normal((self.energy+other.energy)/2)
-       baby.energy=baby.energyTotal
-       baby.walkEn = np.random.normal((self.walkEn+other.walkEn)/2)
-       baby.runEn = np.random.nomral((self.runEn+other.runEn)/2)
-       baby.lifespan = np.random.normal((self.lifespan+other.lifespan)/2)
-       baby.life = baby.lifespan
-       
-       return baby
-       
-      
     def eat(self,food):
         self.energy += (food.life/food.lifetime)*food.size
         if self.energy > self.energyTotal:
@@ -119,17 +71,17 @@ class lHerb:
         
         
         
-class sPlant:
+class Plant:
     '''
-    class for a small plant (dandelion)
+    class for general plant 
     '''
     
     def __init__(self, maxSize=5.0, size=5.0, lifetime=100, life=100, x=0, y=0):
         
-        self.maxSize = maxSize #How large the plant can be        
-        self.size = size #How large the plant is
-        self.lifetime = lifetime #How many time steps the plant can live
-        self.life = life #how many time steps the plant has left
+        self.maxSize = np.random.normal(maxSize) #How large the plant can be        
+        self.size = np.random.normal(size) #How large the plant is
+        self.lifetime = np.random.normal(lifetime) #How many time steps the plant can live
+        self.life = np.random.normal(life) #how many time steps the plant has left
         self.x = x #Its x-axis position
         self.y = y #Its y-axis position
         
@@ -139,20 +91,83 @@ class sPlant:
             if self.size > self.maxSize:
                 self.size = self.maxSize
                 
+    def spread(self):
+        newPlant = type(self)()
+        newPlant.size = np.random.normal(self.size)
+        newPlant.maxSize = np.random.normal(self.maxSize)
+        newPlant.lifetime = np.random.normal(self.lifetime)
+        newPlant.life = newPlant.lifetime
+        newPlant.x = self.x
+        newPlant.y = self.y
+        if bool(random.getrandbits(1)) == True:
+            newPlant.x +=1
+        else:
+            newPlant.x -=1
+        if bool(random.getrandbits(1)) == True:
+            newPlant.y +=1
+        else:
+            newPlant.y -=1
+        return newPlant
+            
         
-        
-        
-        
-class lPlant:
+class Ecosystem:
     '''
-    class for a large plant (bush/tree)
+    Class for entire ecosystem of plants, herbivores, and carnivores
     '''
     
-    def __init__(self, size=50.0, lifetime = 10000, life=10000, x=0, y=0):
+    def __init__(self, fig, sPlants=0, lPlants=0, sHerb=0, lHerb=0, sCarn=0, lCarn=0, size=10):
         
-        self.size = size #How large the plant is
-        self.lifetime = lifetime #How many time steps the plant will live
-        self.life = life #how many time steps the plant has left
-        self.x = x #Its x-axis position
-        self.y = y #Its y-axis position
+        self.sPlants = []
+        #self.lPlants = []
+        #self.sHerb = []
+        #self.lHerb = []
+        #self.sCarn = []
+        #self.lCarn = []
         
+        for i in range(sPlants):
+            self.sPlants.append(Plant(x=np.random.randint(0,size), y=np.random.randint(0,size)))
+        
+        self.fig = fig
+
+        
+    def __getitem__(self,index):
+        return self.sPlants[index]
+        
+    def plot(self):
+        for i in range(len(self.sPlants)):
+            plt.plot(self.sPlants[i].x, self.sPlants[i].y, color='g')
+        
+    
+    def timeStep(self):
+        for i in range(len(self.sPlants)):
+            self.sPlants[i].grow()
+            if np.random.random_sample() < 0.3:
+                self.sPlants.append(self.sPlants[i].spread())
+            self.sPlants[i].life -=1
+        #self.show()
+                
+    def init(self):
+        #fig = plt.figure(figsize=(10,8))
+        self.plot()
+            
+        #plt.show()
+    
+    def animate(self,i):
+        self.timeStep()
+        self.plot()
+        #plt.show()
+    
+    def display(self, frames):
+        ani = animation.FuncAnimation(self.fig, self.animate, frames, init_func=self.init())
+        plt.show()
+        
+    
+        
+        
+    
+figure = plt.figure(figsize=(10,8))
+newEco = Ecosystem(sPlants = 5, fig=figure)
+newEco.display(50)
+#plt.show()
+
+    
