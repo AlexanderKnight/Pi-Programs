3cc51d93cef245364ebc4b8cf0f656d788da771b
Alexander Knight <alexander.knighr@maine.edu>
2016-06-13 19:41:04 -0400

    modified to autorun python and cleared out dead code on weatherScraper.py

--- weatherScraper.py
+++ weatherScraper.py
@@ -1,3 +1,6 @@
+#! /usr/bin/python3
+# weatherScraper.py
+
 import bs4
 import urllib
 import re
@@ -6,7 +9,7 @@ import textMyself
 r = urllib.request.urlopen('http://forecast.weather.gov/MapClick.php?lat=43.665008355852024&lon=-70.30798472131704#.V18VRx9vEXc')
 soup = bs4.BeautifulSoup(r, 'html5lib')
 
-#weather = soup.find_all('div', class_='col-sm-10 forecast-text')
+
 weather = soup.select('#detailed-forecast-body > .row.row-odd.row-forecast > .col-sm-10.forecast-text')
 todayWeather = weather[0].getText()
 
